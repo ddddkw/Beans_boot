24ba1fe2013b28185450decdf140fce485602b93
doukaiwei <2200600836@qq.com>
2024-11-19 17:10:49 +0800

    图片服务相关接口修改

--- src/main/java/com/als/Beans/Blogs/service/impl/ImageServiceImpl.java
+++ src/main/java/com/als/Beans/Blogs/service/impl/ImageServiceImpl.java
@@ -4,6 +4,7 @@ import com.als.Beans.Blogs.entity.Image;
 import com.als.Beans.Blogs.form.ImageForm;
 import com.als.Beans.Blogs.mapper.ImageMapper;
 import com.als.Beans.Blogs.service.ImageService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +19,12 @@ public class ImageServiceImpl extends ServiceImpl<ImageMapper, Image> implements
         Image image = new Image();
         image.setName(imageForm.getName());
         image.setImage(imageForm.getImage());
+        baseMapper.insert(image);
     }
 
     @Override
     public void queryImage(ImageForm imageForm) {
-
+        LambdaQueryWrapper<Image> lambdaQueryWrapper = new LambdaQueryWrapper();
+        lambdaQueryWrapper.eq(Image::getName,imageForm.getName());
     }
 }
